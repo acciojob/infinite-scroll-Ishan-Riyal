ad3ed74eaa0e1b38c15b2d8ea42bd18d63753d6f
Ishan Riyal <ishanriyal07@gmail.com>
2025-06-13 15:45:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-//your code here!
+const infiList = document.getElementById('infi-list');
 
+let itemCount = 0;
+
+function addItems(n) {
+  let html = '';
+	
+  for (let i = 0; i < n; i++) {
+    html += `<li>Item ${++itemCount}</li>`;
+  }
+
+  infiList.innerHTML += html;
+}
+
+addItems(10);
+
+// Add scroll event listener
+infiList.addEventListener('scroll', () => {
+  if (infiList.scrollTop + infiList.clientHeight >= infiList.scrollHeight) {
+    addItems(2);
+  }
+});
